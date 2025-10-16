322e24648317a1b81cd89d8f85081e4bbad0259a
Eklavya Yadav <eklavya.yadav@easydentdental.com>
2025-10-16 23:33:02 +0530

    site fixed

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+# import streamlit as st
+
+# st.set_page_config(page_title="Operations Workbench", layout="wide")
+
+# st.title("Operations Workbench")
+# st.write("Use the buttons below to Generator Reports.")
+
+# # Navigation button
+# if st.button("➡ Go to WIP Hourly Report"):
+#     st.switch_page("pages/wip-hourly.py")  
+
+
+
+
+
+
 import streamlit as st
 
 st.set_page_config(page_title="Operations Workbench", layout="wide")
 
 st.title("Operations Workbench")
-st.write("Use the buttons below to Generator Reports.")
+st.write("Use the buttons below to generate reports.")
 
 # Navigation button
 if st.button("➡ Go to WIP Hourly Report"):
-    st.switch_page("pages/wip-hourly.py")  
+    st.switch_page("wip-hourly")  # <-- Use just the page name (no .py, no folders)
+
+
+
