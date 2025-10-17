c75a5eb5c99699384573c79ad6b80734bb5429e5
Eklavya Yadav <eklavya.yadav@easydentdental.com>
2025-10-17 08:27:08 +0530

    update again

--- main2.py
+++ main2.py
@@ -1,10 +1,34 @@
+# import streamlit as st
+
+# st.set_page_config(page_title="Operations Workbench", layout="wide")
+
+# st.title("Operations Workbench")
+# st.write("Use the buttons below to Generator Reports.")
+
+# # Navigation button
+# if st.button("➡ Go to WIP Hourly Report"):
+#     st.switch_page("pages/wip-hourly.py")  
+
+
+
+
 import streamlit as st
 
 st.set_page_config(page_title="Operations Workbench", layout="wide")
 
 st.title("Operations Workbench")
-st.write("Use the buttons below to Generator Reports.")
+st.write("Use the button below to go directly to the report page.")
 
-# Navigation button
+# When user clicks the button, switch to another page
 if st.button("➡ Go to WIP Hourly Report"):
-    st.switch_page("pages/wip-hourly.py")  
+    st.switch_page("pages/wip-hourly.py")
+
+
+
+
+####################
+
+
+
+
+
