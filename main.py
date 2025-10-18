bec61d2ec43babaf6496f6a2655a73500e5dc4b9
Eklavya Yadav <eklavya.yadav@easydentdental.com>
2025-10-18 13:14:36 +0530

    added daily units button

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #############################################################
 import streamlit as st
 
-st.set_page_config(page_title="Operations Workbench", layout="wide")
+st.set_page_config(page_title="Operation's Workbench", layout="wide")
 
-st.title("Operations Workbench")
+st.title("Operation's Workbench")
 st.write("Use the button below to go directly to the report page.")
 
 # When user clicks the button, switch to another page
-if st.button("➡ Go to WIP Hourly Report"):
+if st.button("➡ WIP Hourly"):
     st.switch_page("pages/wip-hourly.py")
 
+if st.button("➡ Daily Units Report"):
+    st.switch_page("pages/daily-units.py")
 
 
 
